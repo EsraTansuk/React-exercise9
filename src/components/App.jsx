3b979b7e07ff03fab71965875677a15df9ebd54d
esra tansuk <esra.balkis@outlook.com>
2023-01-17 12:02:36 +0300

    second commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import Login from "./Login";
 
-// var isLoggedin = false;
+var isLoggedin = false;
 
-// const currentTime = new Date(2019, 11, 1, 9).getHours();
+const currentTime = new Date(2019, 11, 1, 9).getHours();
 
 
 
 function App() {
   return <div className="container">{
-    {
-      /* Ternary
-    isLoggedin === true ? <h1>Hello</h1> : <Login />
-    and operator
-    currentTime>12 && <h1>Why are you still working?</h1> */}
+  
+    {/* isLoggedin === true ? <h1>Hello</h1> : <Login /> */}
+  
+    currentTime > 12 ? <h1>Why are you still working?</h1> :null
 
   }</div>;
 }
