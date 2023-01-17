cb3a1890a0c1ab2d92e4db14c5359ade8e01ccc6
esra tansuk <esra.balkis@outlook.com>
2023-01-17 11:57:14 +0300

    react conditional rendering

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
 import React from "react";
+import Login from "./Login";
+
+// var isLoggedin = false;
+
+// const currentTime = new Date(2019, 11, 1, 9).getHours();
+
+
 
 function App() {
-  return (
-    <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
+  return <div className="container">{
+    {
+      /* Ternary
+    isLoggedin === true ? <h1>Hello</h1> : <Login />
+    and operator
+    currentTime>12 && <h1>Why are you still working?</h1> */}
+
+  }</div>;
 }
 
 export default App;
